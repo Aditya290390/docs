ac8153060876805c62ca2f842f72896c3e302fce
Aytida-xd <adityapalwe@smallest.ai>
2026-01-08 16:13:17 +0530

    latest

--- content/script.js
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-F339MVM902"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-F339MVM902');
-</script>
